b70374c5edff1ef64b12fa1e9602e0ba95ea7348
Dudu Santos <contato.dududev@outlook.com>
2022-01-26 14:49:56 -0300

    Projeto Orçamento Pessoal dia 2

--- ES6/app1/app.js
+++ ES6/app1/app.js
@@ -1,3 +1,49 @@
+class Despesa {
+    constructor(ano, mes, dia, tipo, descricao, valor) {
+        this.ano = ano
+        this.mes = mes
+        this.dia = dia
+        this.tipo = tipo
+        this.descricao = descricao
+        this.valor = valor
+    }
+
+    validarDados() {
+        for(let i in this) {
+            if(this[i] == undefined || this[i] == '' || this[i] == null) {
+                return false
+            }
+        }
+        return true
+    }
+}
+
+class Bd {
+
+    constructor() {
+        let id = localStorage.getItem('id')
+
+        if(id === null) {
+            localStorage.setItem('id', 0)
+        }
+    }
+
+    getProximoId() {
+        let proximoId = localStorage.getItem('id')
+        return parseInt(proximoId) + 1
+    }
+
+    gravar(d) {
+        let id = this.getProximoId()
+        
+        localStorage.setItem(id, JSON.stringify(d))
+
+        localStorage.setItem('id', id)
+    }
+}
+
+let bd = new Bd()
+
 function cadastrarDespesa() {
 
     let ano = document.getElementById('ano')
@@ -7,5 +53,21 @@ function cadastrarDespesa() {
     let descricao = document.getElementById('descricao')
     let valor = document.getElementById('valor')
 
-    console.log(ano.value, mes.value, dia.value, tipo.value, descricao.value, valor.value)
+    let despesa = new Despesa(
+        ano.value,
+        mes.value,
+        dia.value,
+        tipo.value,
+        descricao.value,
+        valor.value
+    )
+
+    if(despesa.validarDados()) {
+        bd.gravar(despesa)
+        // Dialog de sucesso
+        $('#sucessoGravacao').modal('show')
+    } else {
+        // Dialog de erro
+        $('#erroGravacao').modal('show')
+    }
 }
